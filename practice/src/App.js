a98770c462586a88f5bec963683ef119f14e9eda
Min Pyae Sone Aung <minpyaesone999@gmail.com>
2023-06-28 20:15:18 -0700

    Finished part1 learning and practice

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,36 +1,54 @@
 import {useState} from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
-const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button
-        handleClick={increaseByOne}
-        text="plus"
-      />
-      <Button
-        handleClick={setToZero}
-        text="zero"
-      />
-      <Button
-        handleClick={decreaseByOne}
-        text="minus"
-      />
+      {left}
+<Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+      <p>total {total}</p>
     </div>
   )
 }
