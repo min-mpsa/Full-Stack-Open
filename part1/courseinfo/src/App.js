dca0c50b0190b7789301c993aac9c65295a1d873
Aung <aungm3@student.douglascollege.ca>
2023-06-28 17:31:56 -0700

    Finished exercises 1.3-1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,47 +1,54 @@
-const Header = ({course}) => (
-  <h1>{course}</h1>
-)
+const Header = ({course}) => {
+  console.log({course})
+  return <h1>{course}</h1>
+}
 
-const Part = ({part, exercises}) => (
-  <p>{part} {exercises}</p>
-)
 
-const Content = ({partInfo}) => {
+const Part = ({part, exercises}) => {
+  console.log({part, exercises})
+  return <p>{part} {exercises}</p>
+}
+
+const Content = ({parts}) => {
+  console.log({parts})
   return (
     <div>
-      <Part part={partInfo[0].part} exercises={partInfo[0].exercises}/>
-      <Part part={partInfo[1].part} exercises={partInfo[1].exercises}/>
-      <Part part={partInfo[2].part} exercises={partInfo[2].exercises}/>
+      <Part part={parts[0].name} exercises={parts[0].exercises}/>
+      <Part part={parts[1].name} exercises={parts[1].exercises}/>
+      <Part part={parts[2].name} exercises={parts[2].exercises}/>
     </div>
   )
 }
 
-const Total = ({partInfo}) => (
-  <p>Number of exercises {partInfo[0].exercises+partInfo[1].exercises+partInfo[2].exercises}</p>
-)
+const Total = ({parts}) => {
+  console.log({parts})
+  return <p>Number of exercises {parts[0].exercises+parts[1].exercises+parts[2].exercises}</p>
+}
 
 const App = () => {
-  const course = 'Full Stack application development'
-  const partInfo = [
-    {
-      "part": "Fundamentals of React",
-      "exercises": 10
-    },
-    {
-      "part": "Using props to pass data",
-      "exercises": 7
-    },
-    {
-      "part": "State of a component",
-      "exercises": 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        "name": "Fundamentals of React",
+        "exercises": 10
+      },
+      {
+        "name": "Using props to pass data",
+        "exercises": 7
+      },
+      {
+        "name": "State of a component",
+        "exercises": 14
+      }
+    ]
+  } 
 
   return (
     <div>
-      <Header course={course} />
-      <Content partInfo={partInfo} />
-      <Total partInfo={partInfo} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
---
+++ practice/practice.js
@@ -0,0 +1,46 @@
+
+
+
+/*
+const sum = (p1,p2) => {
+    console.log(p1)
+    console.log(p2)
+    return p1+p2
+}
+
+const square = p => {
+    return p*p
+}
+
+const t = [1,2,3]
+const tSquared = t.map(p => p*p)
+
+  console.log(object1.name)
+  console.log(object1[fieldName])
+  object1.address = 'Helsinki'
+  object1['secret number'] = 12341
+
+const t = [1,2,3,4,5]
+
+const [first,second, ...rest] = t
+
+console.log(first,second)
+console.log(rest)
+
+
+const t = [1,2,3]
+const m2 = t.map(value => '<li>' + value + '<li>')
+console.log(m2)
+
+const t = [1, -1, 3]
+
+t.push(5)
+
+console.log(t.length) // 4 is printed
+console.log(t[1])     // -1 is printed
+
+t.forEach(value => {
+  console.log(value)  // numbers 1, -1, 3, 5 are printed, each to own line
+})
+
+*/       
